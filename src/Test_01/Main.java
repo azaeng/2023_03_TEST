a4939e385c2da26aa9d973040c2daa78bddaec6a
azaeng <aa22548659@gmail.com>
2023-03-10 00:20:03 +0900

    메세지 박스 생성

---
+++ src/Test_01/Main.java
@@ -0,0 +1,8 @@
+package Test_01;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] arg) {
+    	JOptionPane.showMessageDialog(null,"Java Box Test");
+    }
+}
